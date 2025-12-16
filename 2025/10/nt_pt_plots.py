6166c791ddaa486cc847ee3fb128863058626df6
Shawn Zamperini <zamp@login07.chn.perlmutter.nersc.gov>
2025-12-15 16:47:32 -0800

    December update

--- 2025/10/nt_pt_plots.py
+++ 2025/10/nt_pt_plots.py
@@ -1,15 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Paths are using Perlmutter locations since the NT-high-time-res files are
+# kinda too large for my laptop
+
 # Use pygkyl from the actual repo location
 import sys
-sys.path.append("/home/zamp/github/personal_gkyl_scripts/pygkyl")
+sys.path.append("/global/homes/z/zamp/personal_gkyl_scripts/pygkyl")
 import pygkyl
 
 # Inputs here
-which = "PT"  # NT or PT
-plot_param = "ne"
-plot_polproj = True
+which = "NT"  # NT or PT
+plot_param = "flan_nz"
+plot_polproj = False
+polproj_movie = True
+movie_frames = "all"  # "all" or [min, max]
 
 # Don't want Times New Roman, set in pygkyl
 plt.rcdefaults()
@@ -17,15 +22,15 @@ plt.rcdefaults()
 # Setup simulation object with the DIII-D NT/PT configuration
 fileprefix = "gk_d3d_iwl_adapt_source_3x2v_p1"
 if which == "PT":
-	simdir = "/home/zamp/gkyldir/PT-restart-Oct25"
+	simdir = "/global/cfs/cdirs/m3739/gkeyll/gkyl_for_flan/PT-high-time-res"
 	simulation = pygkyl.simulation_configs.import_config("D3D_PT", simdir, 
 		fileprefix)
-	path = '/home/zamp/flandir/pt_v5/pt_v5.nc'
+	path = '/pscratch/sd/z/zamp/flandir/pt_v5/sol_source_v1.nc'
 elif which == "NT":
-	simdir = "/home/zamp/gkyldir/NT-restart-Oct25"
+	simdir = "/global/cfs/cdirs/m3739/gkeyll/gkyl_for_flan/NT-high-time-res"
 	simulation = pygkyl.simulation_configs.import_config("D3D_NT", simdir, 
 		fileprefix)
-	path = '/home/zamp/flandir/nt_v5/nt_v5.nc'
+	path = '/pscratch/sd/z/zamp/flandir/nt_v5/sol_source_v1.nc'
 
 # Time in microseconds
 simulation.normalization.set("t", "mus")
@@ -38,16 +43,24 @@ timeframe = simulation.flanframes[-1]
 # For Gkeyll data
 sim_frames = simulation.available_frames['field']
 
+# Select frames for movies
+frames = simulation.flanframes[:]
+if movie_frames == "all": print("All frames in movie")
+elif type(movie_frames) is list: 
+	frames = frames[movie_frames[0]:movie_frames[1]]
+else: 
+	print("Error: Unrecognized option. movie_frames = {}".format(movie_frames))
+
 # Settings for each plot options
 plot_settings = {
 	"flan_nz": {"clim":[1e-9, 1e-6], "colorScale":"log", "colorMap":"inferno"},
 	"flan_E_X": {"clim":[-1000, 1000], "colorScale":"linear", "colorMap":"coolwarm"},
-	"ne": {"clim":[1e16, 1e20], "colorScale":"log", "colorMap":"inferno"}
+	"ne": {"clim":[1e17, 1e20], "colorScale":"log", "colorMap":"inferno"}
 }
 
 # Poloidal projection plot
 polproj = pygkyl.PoloidalProjection()
-polproj.setup(simulation,nzInterp=24)
+polproj.setup(simulation, nzInterp=24)
 if plot_polproj:
 	print("Generating poloidal projection...")
 	polproj.plot(plot_param, timeFrame=timeframe, 
@@ -55,3 +68,12 @@ if plot_polproj:
 		clim=plot_settings[plot_param]["clim"], 
 		colorMap=plot_settings[plot_param]["colorMap"], 
 		show_inset=False)
+
+# Poloidal projection movie
+if polproj_movie:
+	print("Generating poloidal projection movie...")
+	polproj.movie(plot_param, moviePrefix='/global/homes/z/zamp/figures/flan_ex_mov_', 
+		colorScale=plot_settings[plot_param]["colorScale"],
+		clim=plot_settings[plot_param]["clim"], 
+		colorMap=plot_settings[plot_param]["colorMap"], 
+		timeFrames=frames, show_inset=False)
